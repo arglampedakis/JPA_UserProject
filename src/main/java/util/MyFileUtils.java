cf18872f0380115b2b882bf880673cf68b14a682
glamb <glamb@192.168.1.65>
2020-06-10 22:12:25 +0300

    new branch for upload file feature. added new table in db, generated the entity class, added UploadServlet, upload html form. Created new package and added MyFileUtils

---
+++ src/main/java/util/MyFileUtils.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author glamb
+ */
+public class MyFileUtils {
+
+    public static byte[] convertInputStreamToByteArray(InputStream in) {
+
+        byte[] returnArray = null;
+
+        try {
+            returnArray = new byte[in.available()];
+            in.read(returnArray);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return returnArray;
+    }
+}
